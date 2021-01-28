c23bf67e5b7b6b08378d2d4df54893602b2fe7a6
Scott McGuire <scottmcguire95@hotmail.co.uk>
2021-01-28 22:43:02 +0000

    Initial Commit

---
+++ src/main/java/com/projectbox/jpaentityrelationships/JpaEntityRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.projectbox.jpaentityrelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEntityRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEntityRelationshipsApplication.class, args);
+	}
+
+}
